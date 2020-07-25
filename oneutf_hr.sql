bdad88745424b0a4e6c07f7dfa7040ee5545a811
JayChou <17370180061@163.com>
2020-07-26 00:25:16 +0800

    组织管理

--- oneutf_hr.sql
+++ oneutf_hr.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地数据库
+ Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 50729
+ Source Server Version : 50724
  Source Host           : localhost:3306
- Source Schema         : oneutf_sys
+ Source Schema         : oneutf_hr
 
  Target Server Type    : MySQL
- Target Server Version : 50729
+ Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 11/06/2020 17:14:32
+ Date: 26/07/2020 00:24:00
 */
 
 SET NAMES utf8mb4;
@@ -25,6 +25,34 @@ CREATE TABLE `role_user_ref`  (
   `role_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `user_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_org
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_org`;
+CREATE TABLE `sys_org`  (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT 'id',
+  `org_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名称',
+  `org_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '类型',
+  `telephone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '电话',
+  `fax` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '传真',
+  `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
+  `deptParentId` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '上级部门',
+  `establishTime` datetime(0) NULL DEFAULT NULL COMMENT '成立日期',
+  `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '最后修改者',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '最后修改时间',
+  `is_deleted` bit(1) NULL DEFAULT b'0' COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_org
+-- ----------------------------
+INSERT INTO `sys_org` VALUES ('0ea37eb2e18842eed0d0c0bfb8ea5053', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:19:43', NULL, NULL, b'0');
+INSERT INTO `sys_org` VALUES ('24e0ea47468cf57593f43e63029e8768', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:14:06', NULL, NULL, b'0');
+INSERT INTO `sys_org` VALUES ('33c5bdad12094be89f3e2c103fa37d1a', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:16:24', NULL, NULL, b'0');
 
 -- ----------------------------
 -- Table structure for sys_role
