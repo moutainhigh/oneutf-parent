e0dafd45cbce2198c3d275405b15ebefc6f27c63
oneutf <oneutf>
2020-07-28 22:05:55 +0800

    日期格式化

--- oneutf_hr.sql
+++ oneutf_hr.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : localhost
+ Source Server         : mysql
  Source Server Type    : MySQL
- Source Server Version : 50724
+ Source Server Version : 50727
  Source Host           : localhost:3306
  Source Schema         : oneutf_hr
 
  Target Server Type    : MySQL
- Target Server Version : 50724
+ Target Server Version : 50727
  File Encoding         : 65001
 
- Date: 26/07/2020 01:31:35
+ Date: 28/07/2020 22:05:37
 */
 
 SET NAMES utf8mb4;
@@ -34,11 +34,11 @@ CREATE TABLE `hr_employee`  (
   `id_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '身份证号',
   `dept_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '部门',
   `position_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '岗位',
-  `entry_time` datetime(0) NULL DEFAULT NULL COMMENT '入职日期',
-  `first_work_time` datetime(0) NULL DEFAULT NULL COMMENT '参加工作日期',
+  `entry_date` datetime(0) NULL DEFAULT NULL COMMENT '入职日期',
+  `first_work_date` datetime(0) NULL DEFAULT NULL COMMENT '参加工作日期',
   `staff_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '用工形式',
   `recruit_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '人员来源',
-  `political_lutlook` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '政治面貌',
+  `political_outlook` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '政治面貌',
   `nation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '民族',
   `native_place` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '籍贯',
   `telephone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '联系电话',
@@ -52,7 +52,7 @@ CREATE TABLE `hr_employee`  (
   `highest_degree` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '最高学位',
   `graduation_school` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '毕业院校',
   `major` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '所学专业',
-  `graduation_time` datetime(0) NULL DEFAULT NULL COMMENT '毕业日期',
+  `graduation_date` datetime(0) NULL DEFAULT NULL COMMENT '毕业日期',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
 
@@ -62,13 +62,14 @@ CREATE TABLE `hr_employee`  (
 DROP TABLE IF EXISTS `hr_org`;
 CREATE TABLE `hr_org`  (
   `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT 'id',
-  `org_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名称',
-  `org_type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '类型',
+  `code` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '编号',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '名称',
+  `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '类型',
   `telephone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '电话',
   `fax` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '传真',
-  `describe` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
-  `dept_parent_id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '上级部门',
-  `establish_time` datetime(0) NULL DEFAULT NULL COMMENT '成立日期',
+  `desc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '描述',
+  `dept_parent` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '上级部门',
+  `establish_date` date NULL DEFAULT NULL COMMENT '成立日期',
   `create_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '创建者',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `update_by` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '最后修改者',
@@ -80,9 +81,11 @@ CREATE TABLE `hr_org`  (
 -- ----------------------------
 -- Records of hr_org
 -- ----------------------------
-INSERT INTO `hr_org` VALUES ('0ea37eb2e18842eed0d0c0bfb8ea5053', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:19:43', NULL, NULL, b'0');
-INSERT INTO `hr_org` VALUES ('24e0ea47468cf57593f43e63029e8768', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:14:06', NULL, NULL, b'0');
-INSERT INTO `hr_org` VALUES ('33c5bdad12094be89f3e2c103fa37d1a', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-25 22:16:24', NULL, NULL, b'0');
+INSERT INTO `hr_org` VALUES ('010fef92446bd0d04b398908dfb1096d', '1', '1', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'0');
+INSERT INTO `hr_org` VALUES ('0407a44a02ac02f25fd9aeb9b877259f', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2020-07-22', NULL, NULL, NULL, NULL, b'0');
+INSERT INTO `hr_org` VALUES ('0e34c248c4c3d05e0b05965c758bb2d0', '2', '2', '', '', '', '', '', NULL, '', NULL, '', NULL, b'0');
+INSERT INTO `hr_org` VALUES ('47e9f7ab2a8fe67a7771fcb1897a64eb', NULL, '3', '公司', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'0');
+INSERT INTO `hr_org` VALUES ('bb5b19550d85e3358b80d2e23b8904f2', '3', '4', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'0');
 
 -- ----------------------------
 -- Table structure for role_user_ref
@@ -128,6 +131,6 @@ CREATE TABLE `sys_user`  (
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('35fbbacd32cae2e252232f0288af7345', NULL, NULL, NULL, '2020-06-03 18:10:45', NULL, NULL, b'0');
+INSERT INTO `sys_user` VALUES ('35fbbacd32cae2e252232f0288af7345', 'one', 'hanhan', NULL, '2020-06-03 18:10:45', NULL, NULL, b'0');
 
 SET FOREIGN_KEY_CHECKS = 1;
